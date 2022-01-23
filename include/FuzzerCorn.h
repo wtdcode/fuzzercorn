36fdb1272a64593f851cd3477e73c0f9eb40ec62
lazymio <mio@lazym.io>
2022-01-23 14:21:55 +0100

    Update docs

--- include/FuzzerCorn.h
+++ include/FuzzerCorn.h
@@ -27,30 +27,62 @@ typedef enum FuzzerCornError {
   FUZZERCORN_ERR_UC_ERR,
 } FuzzerCornError;
 
+// Called once per process to initialize everything before fuzzing.
 typedef int (*FuzzerCornInitialize)(uc_engine *Uc, int *Argc, char ***Argv,
                                     void *UserData);
 
+// Called everytime before starting Unicorn.
 typedef bool (*FuzzerCornPlaceInputCallback)(uc_engine *Uc, const uint8_t *Data,
                                              size_t Size, void *UserData);
 
+// Validate whether a Unicorn error is a crash.
 typedef bool (*FuzzerCornValidateCallback)(uc_engine *Uc, uc_err UcErr,
                                            const uint8_t *Data, size_t Size,
                                            void *UserData);
 
+// Used to mutate input **in-place**.
 typedef size_t (*FuzzerCornMutatorCallback)(uc_engine *Uc, uint8_t *Data,
                                             size_t Size, size_t MaxSize,
                                             unsigned int Seed, void *UserData);
 
+// Usede to cross over input and write to Out.
 typedef size_t (*FuzzerCornCrossOverCallback)(
     uc_engine *Uc, const uint8_t *Data1, size_t Size1, const uint8_t *Data2,
     size_t Size2, uint8_t *Out, size_t MaxOutSize, unsigned int Seed,
     void *UserData);
 
+// Specify a range of code to instrument.
 typedef struct {
   uint64_t begin;
   uint64_t end;
 } InstrumentRange;
 
+// The main entry point of the fuzzer.
+// Note this function should be called only **ONCE** per process.
+//
+// @Uc: The Unicorn instance.
+// @Argc: A pointer to argc.
+// @Argv: A pointer to argv array.
+// @Input: The Callback to place input. If it returns false, the unicorn won't be
+//         started. Users also may use this to implement custom fuzzing logic, for
+//         example starting fuzzer in the callback. Always return 0.
+// @Init: The Callback to initialize before fuzzing. Only called once and should always
+//        return 0 whatever happens.
+// @Validate: Validate if an error is a crash. Only get called if unicorn returns an
+//            error by default. If @AlwaysValidate is set to true, it would be called
+//            everytime the emulation is done.
+// @Mutate: Mutate the input **in-place**. Note that setting this pointer to non-null but
+//          don't provide any implementation may have side-effects. If you would not like to
+//          mutate, set it to nullptr.
+// @Cross: Combines two input to new output.
+// @Ranges: Specify the ranges the fuzzer is interested. Only the code within the ranges
+//          would be intrumented. Setting this to nullptr will get all code instrumented.
+// @UserData: User provided data and will be passed to callbacls.
+// @AlwaysValidate: see @Validate.
+// @ExitCode: The program (fuzzer) exit code. Should be returned as the exit code of the
+//            outer program.
+// @CounterCount: The coverage map size. Reduce this can speedup the fuzzing but may cause
+//                more conflicts.
 FUZZER_INTERFACE_VISIBILITY FuzzerCornError FuzzerCornFuzz(
     uc_engine *Uc, int *Argc, char ***Argv, FuzzerCornPlaceInputCallback Input,
     FuzzerCornInitialize Init, FuzzerCornValidateCallback Validate,
